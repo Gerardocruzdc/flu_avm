1ef9c41b048248c3d43e00be96b1a5e7fd2c6d3f
gcrudec <gerardocruzdc@gmail.com>
2026-05-06 19:05:17 +0200

    gcrudec: Widget Dismissible para borrar Listviews

--- lib/presentation/screens/band/bands_screen.dart
+++ lib/presentation/screens/band/bands_screen.dart
@@ -32,17 +32,34 @@ class BandsScreen extends StatelessWidget {
       );
   }
 
-  ListTile _bandTile(Band band) {
-    return ListTile(
-          leading: CircleAvatar(
-            child: Text(band.nomen.substring(0, 2).toUpperCase()),
+  Widget _bandTile(Band band) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        print('Direction: $direction'); 
+        print('id: ${ band.id}');// Handle the dismissal action here
+      },
+      background: Container(
+        padding: EdgeInsets.only(left:18),
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text('Delete Band', style: TextStyle(color: Colors.white),),
           ),
-          title: Text(band.nomen),
-          trailing: Text('${band.numerusVotum}', style: TextStyle(fontSize:20),),
-          onTap: () {
-            print(band.nomen);
-          },
-        );
+        ),
+    
+      child: ListTile(
+            leading: CircleAvatar(
+              child: Text(band.nomen.substring(0, 2).toUpperCase()),
+            ),
+            title: Text(band.nomen),
+            trailing: Text('${band.numerusVotum}', style: TextStyle(fontSize:20),),
+            onTap: () {
+              print(band.nomen);
+            },
+          ),
+    );
   }
 
   addereNovumBand(BuildContext context){
