f200903a8bdef623203d9322e668c4baf05505a2
gcrudec <gerardocruzdc@gmail.com>
2026-05-12 17:14:38 +0200

    gcrudec: Socket.io en Flutter

--- lib/config/entities/band.dart
+++ lib/config/entities/band.dart
@@ -21,5 +21,14 @@ Band copyWith({
     numerusVotum: numerusVotum ?? this.numerusVotum,
     );
   }
+
+  factory Band.fromMap(Map<String, dynamic> obj) {
+    return Band(
+      id: obj['id'] ?? '',
+      nomen: obj['nomen'] ?? '',
+      numerusVotum: obj['numerusVotum']?.toInt() ?? 0,
+    );
+  }
+
 }
 
--- lib/presentation/providers/bands_provider.dart
+++ lib/presentation/providers/bands_provider.dart
@@ -1,10 +1,72 @@
 import 'package:flu_avm/config/config.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-final bandsProvider = StateNotifierProvider<BandsNotifier, List<Band>>((ref) {
+
+enum ServerStatus {
+  // ignore: constant_identifier_names
+  Online,
+  Offline,
+  Connecting
+}
+
+final bandsProvider = StateNotifierProvider<BandsNotifier, BandsState>((ref) {
   return BandsNotifier();
 });
 
+class BandsState {
+  final ServerStatus serverStatus;
+  final IO.Socket socket;
+  final List<Band> bands;
+
+  BandsState({
+    required this.serverStatus,
+    required this.socket,
+    required this.bands
+  });
+
+  BandsState copyWith({
+    ServerStatus? serverStatus,
+    IO.Socket? socket,
+    List<Band>? bands
+  }) => BandsState(
+    serverStatus: serverStatus ?? this.serverStatus,
+    socket: socket ?? this.socket,
+    bands: bands ?? this.bands
+  );
+}
+
+class BandsNotifier extends StateNotifier<BandsState> {
+  BandsNotifier() 
+  : super(BandsState(
+    serverStatus: ServerStatus.Connecting,
+    socket: IO.io(
+      'http://192.168.1.27:3000',
+      IO.OptionBuilder()
+        .setTransports(['websocket'])
+        .enableAutoConnect()
+        .build()),
+    bands: []
+    
+  )) {
+
+    _initConfig();
+  }
+
+  void _initConfig() {
+    state.socket.onConnect(( _ ) {
+      state = state.copyWith(serverStatus: ServerStatus.Online);
+    });
+
+    state.socket.onDisconnect(( _ ) {
+      state = state.copyWith(serverStatus: ServerStatus.Offline);
+    });
+  }
+
+}
+
+
+/*
 class BandsNotifier extends StateNotifier<List<Band>>{
   BandsNotifier() : super([
     Band(id: '1', nomen: 'The Doors', numerusVotum: 5),
@@ -27,3 +89,5 @@ class BandsNotifier extends StateNotifier<List<Band>>{
   }
 
 }
+
+*/
