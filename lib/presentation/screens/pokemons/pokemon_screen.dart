a95fb82ad31814d9ac9e7c2321454eac8e7412cd
gcrudec <gerardocruzdc@gmail.com>
2026-05-24 19:45:42 +0200

    gcrudec: Pokemons finalizados

--- lib/presentation/screens/pokemons/pokemon_screen.dart
+++ lib/presentation/screens/pokemons/pokemon_screen.dart
@@ -1,20 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class PokemonScreen extends StatelessWidget {
+import '../../../config/config.dart';
+import '../../providers/providers.dart';
+
+class PokemonScreen extends ConsumerWidget {
 
   final String pokemonId;
 
   const PokemonScreen({super.key, required this.pokemonId});
 
   @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final pokemonAsync = ref.watch(pokemonProvider(pokemonId));
+
+    return pokemonAsync.when(
+      data: (pokemon) => _PokemonVisum(pokemon: pokemon),
+      error: (error, stackTrace) => _ErrorWidget(nuntius: error.toString()),
+      loading: () => const _LoadingWidget(),
+    );
+  }
+}
+
+class _PokemonVisum extends StatelessWidget {
+
+  final Pokemon pokemon;
+
+  const _PokemonVisum({required this.pokemon});
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Nombre del Pokemon'),
+        title: Text(pokemon.nomen),
       ),
+
       body: Center(
-        child: Text('Datos del pokemon $pokemonId', style: GoogleFonts.russoOne(fontSize: 22), ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 30,
+          children: [
+            Text("Sus habilidades son:", style: GoogleFonts.russoOne(fontSize: 20)),
+            Text(pokemon.facultates.join(', '), style: GoogleFonts.russoOne(fontSize: 22)),
+            Image.network(pokemon.faciemImaginem ?? '', width: 300, height: 300),
+            SizedBox(height: 15),
+            Text("Mide: ${pokemon.altitudo/10} m. y pesa ${pokemon.pondus/10} kg", 
+            style: GoogleFonts.russoOne(fontSize: 22)),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ErrorWidget extends StatelessWidget {
+
+  final String nuntius;
+
+  const _ErrorWidget({required this.nuntius});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Error: $nuntius'),
+      ),
+    );
+  }
+}
+
+class _LoadingWidget extends StatelessWidget {
+
+  const _LoadingWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: CircularProgressIndicator(),
       ),
     );
   }
--- lib/presentation/screens/pokemons/pokemons_screen.dart
+++ lib/presentation/screens/pokemons/pokemons_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-
 import '../../providers/pokemon_index_provider.dart';
 import '../../providers/providers.dart';
 
